fff593bd0a8d5aa011a8c7cc8177a2aa4c4b1a3c
Jon goldberg <jon@palantetech.coop>
2016-11-28 11:46:07 -0500

    Oops, this is the actual mapping managed entity

---
+++ CRM/Upsbatchfile/Mapping.mgd.php
@@ -0,0 +1,14 @@
+<?php
+// Create an export file mapping that declares the main UPS Batch File rows.
+return array(
+  0 => array(
+    'name' => 'UPS_Batch_File',
+    'entity' => 'Mapping',
+    'params' => array(
+      'version' => 3,
+      "name" => "UPS Batch File",
+      "description" => "created by UPS Batch File extension",
+      "mapping_type_id" => "8"
+    ),
+  ),
+);
