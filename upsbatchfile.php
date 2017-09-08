4a41fe897b133464767f06f38b458898c5956eac
Jon goldberg <jon@palantetech.coop>
2017-09-08 12:47:12 -0400

    Add more columns per Nick's request

--- upsbatchfile.php
+++ upsbatchfile.php
@@ -20,42 +20,142 @@ function upsbatchfile_civicrm_export(&$exportTempTable, &$headerRows, &$sqlColum
   if (!$result) {
     return;
   }
-  CRM_Core_Error::backtrace(1, 1);
+  CRM_Core_Error::debug_var('sqlColumns', $sqlColumns);
   $sql = "ALTER TABLE $exportTempTable " .
-    "ADD COLUMN ups_packaging_type CHAR(2) " .
+    "ADD COLUMN contact_name CHAR(255) FIRST" .
+    ",ADD COLUMN ups_packaging_type CHAR(2) " .
+    ",ADD COLUMN customs_value CHAR(1)" .
     ",ADD COLUMN ups_weight CHAR(5)" .
     ",ADD COLUMN ups_length CHAR(4)" .
     ",ADD COLUMN ups_width CHAR(4)" .
     ",ADD COLUMN ups_height CHAR(4)" .
+    ",ADD COLUMN blankT CHAR(1)" .
+    ",ADD COLUMN blankU CHAR(1)" .
+    ",ADD COLUMN blankV CHAR(1)" .
+    ",ADD COLUMN blankW CHAR(1)" .
+    ",ADD COLUMN blankX CHAR(1)" .
+    ",ADD COLUMN ups_service CHAR(2)" .
+    ",ADD COLUMN blankZ CHAR(1)" .
+    ",ADD COLUMN blankAA CHAR(1)" .
+    ",ADD COLUMN blankAB CHAR(1)" .
+    ",ADD COLUMN blankAC CHAR(1)" .
+    ",ADD COLUMN blankAD CHAR(1)" .
+    ",ADD COLUMN blankAE CHAR(1)" .
+    ",ADD COLUMN blankAF CHAR(1)" .
+    ",ADD COLUMN blankAG CHAR(1)" .
+    ",ADD COLUMN blankAH CHAR(1)" .
+    ",ADD COLUMN blankAI CHAR(1)" .
+    ",ADD COLUMN email_notification_1_address CHAR(50)" .
+    ",ADD COLUMN email_notification_1_ship CHAR(1)" .
+    ",ADD COLUMN email_notification_1_exception CHAR(1)" .
+    ",ADD COLUMN email_notification_1_delivery CHAR(1)" .
+    ",ADD COLUMN email_notification_2_address CHAR(50)" .
+    ",ADD COLUMN email_notification_2_ship CHAR(1)" .
     ",ADD COLUMN ups_residential_indicator CHAR(1) AFTER `phone_ext`";
 
   CRM_Core_DAO::singleValueQuery($sql);
 
   $sql = "UPDATE $exportTempTable " .
     "SET ups_packaging_type = 2, " .
+    "customs_value = '', " .
     "ups_weight = 2, " .
     "ups_length = 8, " .
     "ups_width = 6, " .
     "ups_height = 6, " .
     "ups_residential_indicator = 1, " .
+    "contact_name = '', " .
+    "blankT = '', " .
+    "blankU = '', " .
+    "blankV = '', " .
+    "blankW = '', " .
+    "blankX = '', " .
+    "ups_service = 3, " .
+    "blankZ = '', " .
+    "blankAA = '', " .
+    "blankAB = '', " .
+    "blankAC = '', " .
+    "blankAD = '', " .
+    "blankAE = '', " .
+    "blankAF = '', " .
+    "blankAG = '', " .
+    "blankAH = '', " .
+    "blankAI = '', " .
+    "email_notification_1_address = 'test@example.org', " .
+    "email_notification_1_ship = 1, " .
+    "email_notification_1_exception = 1, " .
+    "email_notification_1_delivery = 1, " .
+    "email_notification_2_address = 'donations@calyxinstitute.org', " .
+    "email_notification_2_ship = 1, " .
     "country = 'US'";
   CRM_Core_DAO::singleValueQuery($sql);
 
+  // Get the email address again
+  $sql = "UPDATE $exportTempTable SET email_notification_1_address = email";
+  CRM_Core_DAO::singleValueQuery($sql);
+
   $headerRows[] = "Packaging Type";
+  $headerRows[] = "Customs Value";
   $headerRows[] = "Weight";
   $headerRows[] = "Length";
   $headerRows[] = "Width";
   $headerRows[] = "Height";
+  $headerRows[] = "";
+  $headerRows[] = "";
+  $headerRows[] = "";
+  $headerRows[] = "";
+  $headerRows[] = "";
+  $headerRows[] = "Service";
+  $headerRows[] = "";
+  $headerRows[] = "";
+  $headerRows[] = "";
+  $headerRows[] = "";
+  $headerRows[] = "";
+  $headerRows[] = "";
+  $headerRows[] = "";
+  $headerRows[] = "";
+  $headerRows[] = "";
+  $headerRows[] = "";
+  $headerRows[] = "Email Notification 1 - Address";
+  $headerRows[] = "Email Notification 1 - Ship";
+  $headerRows[] = "Email Notification 1 - Exception";
+  $headerRows[] = "Email Notification 1 - Delivery";
+  $headerRows[] = "Email Notification 2 - Address";
+  $headerRows[] = "Email Notification 2 - Ship";
 
   $sqlColumns['ups_packaging_type'] = 'packaging_type CHAR(2)';
+  $sqlColumns['customs_value'] = 'CHAR(1)';
   $sqlColumns['ups_weight'] = 'CHAR(5)';
   $sqlColumns['ups_length'] = 'CHAR(4)';
   $sqlColumns['ups_width'] = 'CHAR(4)';
   $sqlColumns['ups_height'] = 'CHAR(4)';
+  $sqlColumns['blankT'] = 'CHAR(1)';
+  $sqlColumns['blankU'] = 'CHAR(1)';
+  $sqlColumns['blankV'] = 'CHAR(1)';
+  $sqlColumns['blankW'] = 'CHAR(1)';
+  $sqlColumns['blankX'] = 'CHAR(1)';
+  $sqlColumns['ups_service'] = 'CHAR(1)';
+  $sqlColumns['blankZ'] = 'CHAR(1)';
+  $sqlColumns['blankAA'] = 'CHAR(1)';
+  $sqlColumns['blankAB'] = 'CHAR(1)';
+  $sqlColumns['blankAC'] = 'CHAR(1)';
+  $sqlColumns['blankAD'] = 'CHAR(1)';
+  $sqlColumns['blankAE'] = 'CHAR(1)';
+  $sqlColumns['blankAF'] = 'CHAR(1)';
+  $sqlColumns['blankAG'] = 'CHAR(1)';
+  $sqlColumns['blankAH'] = 'CHAR(1)';
+  $sqlColumns['blankAI'] = 'CHAR(1)';
+  $sqlColumns['email_notification_1_address'] = 'CHAR(50)';
+  $sqlColumns['email_notification_1_ship'] = 'CHAR(1)';
+  $sqlColumns['email_notification_1_exception'] = 'CHAR(1)';
+  $sqlColumns['email_notification_1_delivery'] = 'CHAR(1)';
+  $sqlColumns['email_notification_2_address'] = 'CHAR(50)';
+  $sqlColumns['email_notification_2_ship'] = 'CHAR(1)';
 
-  // Residential Indicator is a special case, it needs splicing into the middle.
+  // Handle the columns that need splicing into the middle of the export.
   array_splice($headerRows, 10, 0, array("Residential Indicator"));
   array_splice_preserve_keys($sqlColumns, 10, 0, array('ups_residential_indicator' => 'CHAR(1)'));
+  array_splice($headerRows, 0, 0, array("Contact Name"));
+  array_splice_preserve_keys($sqlColumns, 0, 0, array('contact_name' => 'CHAR(255)'));
 
 }
 function array_splice_preserve_keys(&$input, $offset, $length = NULL, $replacement = array()) {
